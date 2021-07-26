301bf8d6daa425e44eb78d9ca203879c2932c8ee
DinhNgocThai <dinhngocthai0205@gmail.com>
2021-07-26 20:35:34 +0700

    add husky, prettier, lint-staged

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-const path= require('path');
-const morgan= require('morgan');
+const path = require('path');
+const morgan = require('morgan');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
-const port=3001;
-app.use(express.static(path.join(__dirname,'public')));
+const port = 3001;
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 //  handle bars
 
-app.engine('hbs', handlebars({
-     extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
+app.get('/', (req, res) => {
+    return res.render('home');
+});
+app.get('/news', (req, res) => {
+    return res.render('news');
+});
+app.get('/search', (req, res) => {
+    console.log(req.query);
 
-app.get('/', ( req, res) => {
-    
-     return res.render('home');
-})
-app.get('/news', ( req, res) => {
-    
-     return res.render('news');
-})
-app.listen(port, ()=> console.log(`Example app listening at  http://localhost:${port}`)); 
+    return res.render('search');
+});
+app.listen(port, () =>
+    console.log(`Example app listening at  http://localhost:${port}`),
+);
